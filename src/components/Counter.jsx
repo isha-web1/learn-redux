a3cb41287816cb23c0ec30abdcb3e08125ad555f
isha-web1 <isha.web1@gmail.com>
2023-10-05 12:28:30 +0600

    simple increment decrement action payload used

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByValue } from '../redux/features/counter/CounterSlice';
 
 const Counter = () => {
 
  const {count} = useSelector((state) => state.counter);
+ const dispatch = useDispatch();
  console.log(count)
 
     return (
         <div>
-            <button>increment</button>
+            <button onClick={()=>dispatch(increment())}>increment</button>
+            <br />
+            {/*used action payload for incrment by 4  */}
+            <button onClick={()=>dispatch(incrementByValue(4))}>increment by 4</button>
             <div>
                 <h1>{count}</h1>
             </div>
-            <button>decrement</button>
+            <button onClick={()=>dispatch(decrement())}>decrement</button>
         </div>
     );
 };
