8738fc99d4bb1979c40638ef5d9a9e6bbb2b3877
isha-web1 <isha.web1@gmail.com>
2023-10-05 23:14:39 +0600

    custom middleware added

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -2,10 +2,13 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './features/counter/CounterSlice'
+// import logger from 'redux-logger';
+import logger from './middlewares/logger';
 const store = configureStore({
     reducer: {
         counter : counterReducer
     },
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
   })
 
   export default store;
---
+++ src/redux/middlewares/logger.js
@@ -0,0 +1,8 @@
+const logger = (store) => (next) => (action) => {
+  
+    const currentState = store.getState()
+   console.log(currentState)
+
+};
+
+export default logger;
